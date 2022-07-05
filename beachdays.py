85aa989f88163a7e0382dfa350da33393389bb04
Pedro "Totty" Sousa <pedrosousa084@hotmail.com>
2022-07-06 00:33:20 +0100

    most likely done

--- beachdays.py
+++ beachdays.py
@@ -1,26 +1,31 @@
-
-# Weather API Specifications
-
-# It takes a city and an optional flag to display the output in celsius or fahrenheit as input;
-# We call a weather API to fetch the specified city weather data;
-# We save the fetched data into a CSV file for model training;
+# Library Imports
 
 import json
 import os
 import csv
 import sys
-from urllib import error, parse, request
-from pprint import pp
+import pandas as pd
+from urllib import error, request
+
+# File Imports
 
 import config  # File Containing the API KEY
 import predictionModel  # Training Model
 
+# Global Variables
+
 BASE_API_URL = "http://api.openweathermap.org/data/2.5/weather"
 
+# Functions
 
 def city_checker():
+
+    # Verifies if a city has been saved in the city.txt file or not
+
     if(os.path.exists("city.txt")):
         return get_city()
+
+    # If the file doesn't exist, we ask the user to enter a city and save it in the city.txt file
 
     else:
         city = input("What city do you want to predict beach days?\n")
@@ -28,10 +33,14 @@ def city_checker():
         cityfile.write(city)
         cityfile.close()
 
+    # Then we return whatever city is in the file
+
     return get_city()
 
 
 def get_city():
+
+    # Read the city from the city.txt file and return it
 
     cityfile = open("city.txt", "r")
     city = cityfile.read()
@@ -42,12 +51,16 @@ def get_city():
 
 def weather_query(city):
 
+    # Build the query url to fetch the weather data
+
     url = (f"{BASE_API_URL}?q={city}&units=metric&appid={config.APIKEY}")
 
     return url
 
 
 def get_weather_data(query_url):
+
+    # Fetch the weather data from the API
 
     try:
         query_call = request.urlopen(query_url)
@@ -61,8 +74,11 @@ def get_weather_data(query_url):
 
 def api_to_cvs_data(weather_data, beachday):
 
+    # We pick the data we want to save in the csv file, make it into a list return it
+
     csv_data = [
         weather_data['weather'][0]['description'],
+        weather_data['dt'],
         weather_data['main']['temp'],
         weather_data['main']['pressure'],
         weather_data['main']['humidity'],
@@ -72,46 +88,73 @@ def api_to_cvs_data(weather_data, beachday):
 
     return csv_data
 
-
 def save_to_csv(weather_data, beachday):
+
+    # Write into the csv file the previously prepared data using the csv module
 
     weathercsv = open("weatherdata.csv", "a", newline='')
     writer = csv.writer(weathercsv)
     writer.writerow(api_to_cvs_data(weather_data, beachday))
     weathercsv.close()
 
+def api_to_dataframe(weather_data):
+
+    # Gotta be sure to make a dictionary with the syntax: {'column': value (inside a list)}
+
+    df_data = {
+        'desc' : [weather_data['weather'][0]['description']],
+        'daytime' : [weather_data['dt']],
+        'temperature' : [weather_data['main']['temp']],
+        'pressure' : [weather_data['main']['pressure']],
+        'humidity' : [weather_data['main']['humidity']],
+        'wind_str'  :[weather_data['wind']['speed']],
+        'wind_deg' : [weather_data['wind']['deg']]
+        }
+
+    # So when converting the dictionary into a dataframe, it will be in the desired format
+    
+    df_data = pd.DataFrame(df_data)
+
+    return df_data
+
+
 def predictBeachDay(weather_data):
 
-    # Function to load and preprocess dataset
+    # Convert today's data into a dataframe
 
-    weatherEncoded = predictionModel.loadPreprocessData()
+    df_data = api_to_dataframe(weather_data)
+
+    # Function to load and preprocess dataset (we return the encoder to encode today's data in the same manner)
+
+    weatherEncoded, encoder = predictionModel.loadPreprocessData()
 
     # Function that calls and fits the prediction model
 
     DecisionModel = predictionModel.modelTraining(weatherEncoded)
 
-    # Function that predicts with the weather_data of today
+    # Encode today's data
 
-    prediction = DecisionModel.predict(weather_data) # Work in progress
+    df_data['desc'] = encoder.fit_transform(df_data['desc'])
 
-    return prediction # Boolean
+    # We predict if today is a beach day or not and return a boolean value
+
+    prediction = DecisionModel.predict(df_data)
+
+    return prediction
 
 
 if __name__ == "__main__":
 
-    while True:
-        beachday = input("Is it a beach day? (True of False): ")
-        if (beachday == 'True' or beachday == 'False'):
-            break
-        else:
-            print('Be sure to write "True" or "False".')
+    # Get the city from the city.txt file or ask the user to enter a city
 
     city = city_checker()
+
+    # Get the weather data from the API
 
     query = weather_query(city)
     weather_data = get_weather_data(query)
 
-    save_to_csv(weather_data, beachday)
+    # Ask if the user wants to predict the beach day or not, and if so, predict it
 
     while True:
 
@@ -119,7 +162,7 @@ if __name__ == "__main__":
 
         if (testday == 'Yes'):
 
-            beachdaypred = predictBeachDay(api_to_cvs_data(weather_data, beachday))
+            beachdaypred = predictBeachDay(weather_data)
 
             if beachdaypred: 
                 print("It's a beach day! Enjoy!") 
@@ -128,6 +171,25 @@ if __name__ == "__main__":
             break
 
         elif (testday == 'No'):
+            break
+
+        else:
+            print('Be sure to write "Yes" or "No".')
+
+    # Now ask if it is a beach day or not, and if so, save it in the csv file for future predictions
+            
+    while True:
+
+        beachday = input("Is it a beach day? (Yes of No): ")
+
+        if (beachday == 'Yes'):
+            save_to_csv(weather_data, True)
+            print("Data saved for future predictions!.")
+            break
+
+        elif (beachday == 'No'):
+            save_to_csv(weather_data, False)
+            print("Data saved for future predictions!.")
             break
 
         else:
